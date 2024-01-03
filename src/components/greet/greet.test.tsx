bf467f64edb898a7f8ffbdf4c5fc64144e5d6a6d
kunal.pandey <kunal.pandey@prolitus.com>
2024-01-03 21:37:45 +0530

    greet part done

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,19 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
-test('Greet renders correctly', () => {
-    render(<Greet/>)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
-})
-
-test('Greet renders with a name', () => {
-    render(<Greet name='Kunal'/>)
-    const textElement = screen.getByText(/hello kunal/i)
-    expect(textElement).toBeInTheDocument()
+describe('Greet', () => {
+    
+    test('renders correctly', () => {
+        render(<Greet/>)
+        const textElement = screen.getByText(/hello/i)
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    describe('Nested', () => {
+        test('renders a name', () => {
+            render(<Greet name='Kunal'/>)
+            const textElement = screen.getByText(/hello kunal/i)
+            expect(textElement).toBeInTheDocument()
+        })
+    })
 })
