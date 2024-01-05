d1b65d97036c54ce52b1a033642941d3139a7fb5
kunal.pandey <kunal.pandey@prolitus.com>
2024-01-05 11:40:22 +0530

    pointer and keyboard events done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Application } from './components/application/application';
+import { Counter } from './components/counter/counter';
 
 function App() {
   return (
     <>
-    <Application/>
+    {/* <Application/> */}
+    <Counter/>
     </>
   );
 }
