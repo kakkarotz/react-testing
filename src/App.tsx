ec4d12a5506ccb0da6c69b019e10eb26c15bcf59
kunal.pandey <kunal.pandey@prolitus.com>
2024-01-05 22:27:32 +0530

    providers testing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import './App.css';
-import { Application } from './components/application/application';
-import { Counter } from './components/counter/counter';
+import './App.css'
+import { Application } from './components/application/application'
+// import { CounterTwo } from './components/counter-two/CounterTwo'
+// import { Counter } from './components/counter/counter'
+import { MuiMode } from './components/mui/mui-mode';
+// import { Skills } from './components/skills/Skills'
+// import { Users } from './components/users/Users'
+import { AppProviders } from './providers/AppProviders';
 
 function App() {
   return (
-    <>
-    {/* <Application/> */}
-    <Counter/>
-    </>
-  );
+    <AppProviders>
+      
+      <div className="App">
+        {/* <Application />
+        <Skills skills={['HTML', 'CSS']} />
+        <Counter /> */}
+        {/* <CounterTwo count={1} /> */}
+        {/* <Users /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
+  )
 }
 
-export default App;
+export default App
